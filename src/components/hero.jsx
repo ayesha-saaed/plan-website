c6e8e98ab763f5758f560bc2c4dcbb9eb9a58c18
Your Name <ayeshrao2004@gmail.com>
2024-09-14 07:50:54 -0700

    lecture#3

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import background from "@/assests/image/jpg/image-01.jpg"
-
+import styles from "@/styles/components/hero.module.css"
 
 const Hero = () =>{
     return(
         <>
-<h1>Plan.<br/>Management.<br/>Build.</h1>
+          <div className={styles.wrapper}>
+<h1 className={styles.heroText}>Plan.<br/>Manage.<br/>Build.</h1>
 <div></div>
 <h3>Our Difference Is In Finishing</h3>
 <button>work</button>
-<Image src={background} widht={3840} height={2160} alt="hero image" />
+</div>
+<Image className={styles.heroImage} src={background} widht={3840} height={2160} alt="hero image" />
         </>
     );
 
--- src/components/showcase.jsx
+++ src/components/showcase.jsx
@@ -4,6 +4,14 @@ import Dropbox from "@/assests/logos/png/dropbox.png"
 import Monday from "@/assests/logos/png/monday_logo.png"
 import Ncr from "@/assests/logos/png/ncr.png"
 import Rakuten from "@/assests/logos/png/rakuten_logo.png"
+import User1 from "@/assests/image/jpg/user-01.jpg"
+import User2 from "@/assests/image/jpg/user-02.jpg"
+import User3 from "@/assests/image/jpg/user-03.jpg"
+import User4 from "@/assests/image/jpg/user-04.jpg"
+
+
+
+
 
 
 
@@ -22,19 +30,20 @@ const Showcase = () => {
                 <Image src={Rakuten} width={56} height={36} alt="" />
         </div>
 
-        </>
-    );
-}
-/*<h1>To enhance the growth of your website, it{"'"}s essential to expedite the release of new pages.</h1>
+
+<h1>To enhance the growth of your website, it{"'"}s essential to expedite the release of new pages.</h1>
             <div>
-                <Image src={User01} width={156} height={136} alt="" />
-                <Image src={User02} width={156} height={136} alt="" />
-                <Image src={User03} width={156} height={136} alt="" />
-                <Image src={User04} width={156} height={136} alt="" />
+                <Image src={User1} width={156} height={136} alt="" />
+                <Image src={User2} width={156} height={136} alt="" />
+                <Image src={User3} width={156} height={136} alt="" />
+                <Image src={User4} width={156} height={136} alt="" />
             </div>
             <h1>/</h1>
             <div>
                 <h3>We Received A Great Amoun Of Work in Just 2 Weeks Timeline!</h3>
                 <span>Daniel, Design lead @Google</span>
-            </div>*/
+            </div>
+              </>
+            );
+        }
 export default Showcase;
